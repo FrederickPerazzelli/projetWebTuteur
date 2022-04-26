94c303693fbaea3513133499f397e934c349a9fc
Jean-Nyckolas Roy <jnroy27@gmail.com>
2022-04-26 11:10:07 -0400

    Ajout des entêtes

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,4 +1,16 @@
 <?php
+/****************************************
+ Fichier : UserController.php
+ Auteur : Jean-Nyckolas Roy
+ Fonctionnalité : A2, A3
+ Date : 2022-04-21
+ Vérification :
+ Date Nom Approuvé
+ =========================================================
+ Historique de modifications :
+ 2022-04-21 - Jean-Nyckolas - Ajout de la route pour la liste d'utilisateurs
+ =========================================================
+****************************************/
 
 namespace App\Controller;
 
@@ -11,7 +23,7 @@ use Doctrine\ORM\EntityManagerInterface;
 
 class UserController extends AbstractController
 {
-    #[Route('/', name: 'users')]
+    #[Route('/users', name: 'users')]
     public function index(EntityManagerInterface $em): Response
     {
         $users = $em->getRepository(User::class)->findAll();
